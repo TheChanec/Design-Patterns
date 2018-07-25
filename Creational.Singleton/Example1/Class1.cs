253739755b1f7a5b269e55ab6778699af167b8dc
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-25 14:45:23 -0500

    Simgleton Example

---
+++ Creational.Singleton/Example1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Creational.Singleton.Example1.Models.Singletons;
+
+namespace Creational.Singleton.Example1
+{
+
+    class Class1
+    {
+        /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary>
+
+        Class1()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
