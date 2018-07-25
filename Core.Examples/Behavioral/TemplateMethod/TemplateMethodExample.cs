acff9e6ec423183ad30853d179d49e422b8e42e9
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-25 12:47:13 -0500

    Template Method Example

---
+++ Core.Examples/Behavioral/TemplateMethod/TemplateMethodExample.cs
@@ -0,0 +1,96 @@
+﻿
+using System;
+ 
+namespace Core.Examples.Behavioral.TemplateMethod
+{
+    /// <summary>
+
+    /// MainApp startup class for Real-World 
+
+    /// Template Design Pattern.
+
+    /// </summary>
+
+    class TemplateMethodExample
+
+    {
+        /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary>
+
+        TemplateMethodExample()
+        {
+            AbstractClass aA = new ConcreteClassA();
+            aA.TemplateMethod();
+
+            AbstractClass aB = new ConcreteClassB();
+            aB.TemplateMethod();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'AbstractClass' abstract class
+
+    /// </summary>
+
+    abstract class AbstractClass
+
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+
+        // The "Template method"
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine("");
+        }
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteClass' class
+
+    /// </summary>
+
+    class ConcreteClassA : AbstractClass
+
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+        }
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteClass' class
+
+    /// </summary>
+
+    class ConcreteClassB : AbstractClass
+
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+        }
+    }
+}
