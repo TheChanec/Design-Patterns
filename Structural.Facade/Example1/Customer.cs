a19b8a2f50cfe4b96c23f970daa48048381ccd4b
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-16 10:03:56 -0500

    Facade example

---
+++ Structural.Facade/Example1/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structural.Facade.Example1
+{
+    class Customer
+    {
+        private string _name;
+
+        // Constructor
+
+        public Customer(string name)
+        {
+            this._name = name;
+        }
+
+        // Gets the name
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
---
+++ Structural.Facade/Example1/Loan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structural.Facade.Example1
+{
+    class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+}
