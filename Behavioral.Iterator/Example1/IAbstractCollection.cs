0df03e1392636830dde3e1db3f3d9b3cff42488e
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-16 10:04:49 -0500

    Iterator

---
+++ Behavioral.Iterator/Example1/IAbstractCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Behavioral.Iterator.Example1
+{
+    interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+}
