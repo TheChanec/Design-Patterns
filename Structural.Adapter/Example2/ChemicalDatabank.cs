d9db6a554a417f9de733e4566ad101608de2ee19
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-16 10:04:20 -0500

    Adapter example

---
+++ Structural.Adapter/Example2/ChemicalDatabank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structural.Adapter.Example2
+{
+    class ChemicalDatabank
+    {
+        internal float GetCriticalPoint(string chemical, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double GetMolecularWeight(string chemical)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string GetMolecularStructure(string chemical)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
